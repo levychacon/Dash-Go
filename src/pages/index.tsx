73781433d72f12708af55f0c2be382c38002f40d
Levy Chacon <levychacon@yahoo.com>
2021-09-07 17:05:58 -0300

    pagina de sign in construida

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
-import Head from 'next/head'
-import Image from 'next/image'
 
-export default function Home() {
+import { Flex, Button, Stack } from "@chakra-ui/react";
+import { Input } from "../components/form/input";
+
+export default function SignIn() {
   return (
-    <h1>Dashgo</h1>
-  )
-    
+    <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input name={"email"} label={"Email"} type={"email"} />
+
+          <Input name={"password"} label={"Senha"} type={"password"} />
+        </Stack>
+
+        <Button type="submit" marginTop="6" colorScheme="pink">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
